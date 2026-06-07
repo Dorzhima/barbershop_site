e2ee1e6b5b73812e4c17df4e9ea1955472eae5dd
Dorzhima <adorzhima@bk.ru>
2026-06-07 20:49:34 +0000

    Initial commit

--- barbershop/models.py
+++ barbershop/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Service(models.Model):
     name = models.CharField(max_length=100, unique=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
